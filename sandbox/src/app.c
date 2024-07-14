6b970d416eb3150489a165e3ca25b2506c209c98
Konrad Karas <konradkar2@gmail.com>
2024-07-15 01:19:57 +0200

    blinky

--- sandbox/src/app.c
+++ sandbox/src/app.c
@@ -3,15 +3,17 @@
 #include <libopencm3/stm32/f7/rcc.h>
 
 #define LED_PORT (GPIOB)
-#define LED_PIN  (GPIO14)
+#define LED_RED_PIN  (GPIO14)
+#define LED_BLUE_PIN  (GPIO7)
 
 static void rcc_setup(void) {
-  rcc_clock_setup_hsi(&rcc_3v3[RCC_CLOCK_3V3_72MHZ]);
+  rcc_clock_setup_hsi(&rcc_3v3[RCC_CLOCK_3V3_216MHZ]);
 }
 
 static void gpio_setup(void) {
   rcc_periph_clock_enable(RCC_GPIOB);
-  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_RED_PIN);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_BLUE_PIN);
 }
 
 static void delay_cycles(uint32_t cycles) {
@@ -25,8 +27,9 @@ int main(void) {
   gpio_setup();
 
   while (1) {
-    gpio_toggle(LED_PORT, LED_PIN);
-    delay_cycles(7200000 / 4);
+    gpio_toggle(LED_PORT, LED_RED_PIN);
+    gpio_toggle(LED_PORT, LED_BLUE_PIN);
+    delay_cycles(72000000 / 4);
   }
 
   // Never return
