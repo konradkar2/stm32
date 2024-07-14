8b801813df05f9a5eeab8fa4207158579887d1a3
Konrad Karas <konradkar2@gmail.com>
2024-07-15 01:11:05 +0200

    works!

--- sandbox/src/app.c
+++ sandbox/src/app.c
@@ -2,15 +2,15 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/f7/rcc.h>
 
-#define LED_PORT (GPIOA)
-#define LED_PIN  (GPIO5)
+#define LED_PORT (GPIOB)
+#define LED_PIN  (GPIO14)
 
 static void rcc_setup(void) {
-  rcc_clock_setup_pll(&rcc_3v3[RCC_CLOCK_3V3_72MHZ]);
+  rcc_clock_setup_hsi(&rcc_3v3[RCC_CLOCK_3V3_72MHZ]);
 }
 
 static void gpio_setup(void) {
-  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
 
@@ -26,7 +26,7 @@ int main(void) {
 
   while (1) {
     gpio_toggle(LED_PORT, LED_PIN);
-    delay_cycles(72000000 / 4);
+    delay_cycles(7200000 / 4);
   }
 
   // Never return
